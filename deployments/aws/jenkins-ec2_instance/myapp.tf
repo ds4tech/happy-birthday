256c51625b5325f06751b85daf20210b4c4c6440
Mateusz Szymczyk <zwierzak@MacBook-Pro-MacOS.local>
2020-01-11 13:39:23 +0100

    apt-get install docker

--- deployments/aws/jenkins-ec2_instance/myapp.tf
+++ deployments/aws/jenkins-ec2_instance/myapp.tf
@@ -4,6 +4,7 @@ data "template_file" "myapp-task-definition-template" {
   template = file("templates/app.json.tpl")
   vars = {
     REPOSITORY_URL = replace(aws_ecr_repository.myapp.repository_url, "https://", "")
+    APP_VERSION    = var.MYAPP_VERSION
   }
 }
 
@@ -12,6 +13,7 @@ resource "aws_ecs_task_definition" "myapp-task-definition" {
   container_definitions = data.template_file.myapp-task-definition-template.rendered
 }
 
+# load balancer
 resource "aws_elb" "myapp-elb" {
   name = "myapp-elb"
 
