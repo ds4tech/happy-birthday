4d17e6631eac828cdaae2257894973e0bdea9cfb
Mateusz Szymczyk <zwierzak@MacBook-Pro-MacOS.local>
2020-01-11 16:15:00 +0100

    update

--- deployments/aws/ecs-jenkins/scripts/jenkins-init.sh
+++ deployments/aws/ecs-jenkins/scripts/jenkins-init.sh
@@ -50,8 +50,8 @@ pip install awscli
 
 # install terraform
 wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
-&& unzip -o terraform_$${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
-&& rm terraform_$${TERRAFORM_VERSION}_linux_amd64.zip
+&& unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
+&& rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 
 # install go-lang
 apt-get install -y golang-go
