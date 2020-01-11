11bc0e3933aad44f9fabb1c9f11e964e4c981801
Mateusz Szymczyk <zwierzak@MacBook-Pro-MacOS.local>
2020-01-11 14:56:25 +0100

    python3

--- deployments/aws/jenkins-ec2_instance/scripts/jenkins-init.sh
+++ deployments/aws/jenkins-ec2_instance/scripts/jenkins-init.sh
@@ -43,8 +43,7 @@ service jenkins restart
 
 # install pip
 wget -q https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
-#python3 get-pip.py
+python3 get-pip.py
 rm -f get-pip.py
 # install awscli
 pip install awscli
