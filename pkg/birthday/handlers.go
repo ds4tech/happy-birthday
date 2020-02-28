421a579a5b66af4f409293597f4bc4bff4816a0c
Mateusz Szymczyk <zwierzak@MacBook-Pro-MacOS.local>
2020-02-28 21:27:56 +0100

    remove functions Counter and Parser

--- pkg/birthday/handlers.go
+++ pkg/birthday/handlers.go
@@ -6,9 +6,7 @@ import (
 	"html"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
-	"sync"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -83,29 +81,4 @@ func SaveSmbsName(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	*/
-}
-
-var mu sync.Mutex
-var count int
-
-func Counter(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	count++
-	fmt.Fprintf(w, "Number of request equals: %d\n", count)
-	mu.Unlock()
-}
-
-func Parser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
-	for k, v := range r.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-	}
-	fmt.Fprintf(w, "Host = %q\n", r.Host)
-	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
-	if err := r.ParseForm(); err != nil {
-		log.Print(err)
-	}
-	for k, v := range r.Form {
-		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
-	}
 }
--- pkg/birthday/handlers_test.go
+++ pkg/birthday/handlers_test.go
@@ -4,7 +4,6 @@ import (
     "net/http"
     "net/http/httptest"
     "testing"
-    _ "io"
 )
 
 func TestIndex(t *testing.T) {
